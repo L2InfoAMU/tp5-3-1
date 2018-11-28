cdbfa01db5ae1b030126516fa1695e21fb679b36
keopha <keopha.haze@gmail.com>
2018-11-28 11:05:08 +0100

    Added constructor public BruteRasterImage(Color color[][])       getRepresentation()

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,6 +1,9 @@
 package image;
+import util.Matrices.*;
 
 import javafx.scene.paint.Color;
+
+import static util.Matrices.*;
 
 public class BruteRasterImage implements Image{
 
@@ -11,18 +14,31 @@ public class BruteRasterImage implements Image{
     public BruteRasterImage(Color color, int width, int height){
         this.width = width;
         this.height = height;
-        Color[][] pixels = new Color[height][width];
-        for(int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        createRepresentation();
+        for(int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 pixels[i][j] = color;
             }
         }
     }
 
+    public BruteRasterImage(Color[][] colors){
+        requiresNonNull(colors);
+        requiresNonZeroDimensions(colors);
+        requiresRectangularMatrix(colors);
+        createRepresentation();
+        this.width = getRowCount(colors);
+        this.height = getColumnCount(colors);
+        for(int i = 0; i < this.width; i++){
+            for(int j = 0; j < this.height; j++){
+                pixels[i][j] = colors[i][j];
+            }
+        }
+    }
 
-
-    /**Color getPixelColor(int x, int y);
-    int getWidth();
-    int getHeight();**/
+    public void createRepresentation(){
+        Color[][] pixels = new Color[width][height];
+    }
+    
 
 }
